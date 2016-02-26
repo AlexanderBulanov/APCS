771860d1386b07e3c7222656699a1b3d472c6ddd
AlexanderBulanov <alexanderbulanovcodes@gmail.com>
2016-02-26 12:06:42 -0800

    Create SortingTools.java
    General class to extend basic sorting functions from

---
+++ SortingTools.java
@@ -0,0 +1,19 @@
+//Alexander Bulanov, Adam Barszczak
+import java.util.Scanner;
+
+public class SortingTools {
+	public int[] nums;
+
+	public static void populateArray(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = (int) Math.ceil(Math.random()*nums.length);
+		}
+	}
+
+	public static void printArray(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print(nums[i] + " ");
+		}
+	}
+	
+}
